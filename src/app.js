7083a1b14bf4f684a81243a91ec691bb9e416b34
jbenjaminy <young.benjamin@gmail.com>
2017-01-31 11:17:57 -0600

    nests the header component in the app component and updates reducers in .../reducers/index.js

--- src/app.js
+++ src/app.js
@@ -6,17 +6,25 @@ import { View } from 'react-native';
 // react-redux library is the communication glue between the two frameworks
 import { Provider } from 'react-redux';
 // creates our store object
+/* Whenever we are working with redux and we hear the word 'data' we need to be
+thinking in terms of the reducers' */
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
     return (
-        {/*
+        /*
             Created a store and passed it to the Provider tag, also passed the
             reducers to the store.
-        */}
+
+            *Provider tag can only have one single child component. The child
+            component can have any number of children.
+        */
         <Provider store={createStore(reducers)}>
-            <View />
+            <View>
+                <Header headerText="Tech Stack" />
+            </View>
         </Provider>
 
     );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,22 @@
 import { combineReducers } from 'redux';
 
+/* DIFFERENT TYPES OF REDUCERS - THINKING PROCESS
+    - what are the different variables/pieces of data for this app?
+        - list of libraries.
+        - currently selected library (most recent on the user tapped).
+    => we could have two reducers:
+        - 'library reducer'
+            e.g., [
+                { id: 1, name: 'React' },
+                { id: 2, name: 'Redux' }
+            ]
+            
+        - 'selection reducer'
+            e.g., 1
+
+            this is a separate reducer to just keep track of what item from the
+            list to expand (by storing its id).
+*/
 export default combineReducers({
     libraries: () => []
 });
