318061a2af78e390067143cc7d8919ff4f5bbff3
jbenjaminy <young.benjamin@gmail.com>
2017-01-31 15:42:57 -0600

    adds connect functionand binds actions to ListItem

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -3,7 +3,17 @@
  *  needs to know whether it is selected or not, so must get access to state */
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+/*  Previously used the connect helper to get access to the application state,
+    but can also use the connect helper to get access to an action creator. */
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+/*  Inside of actions file, might have many different actions creators, whenever
+    you want to export many things, just use the export keyword and not
+    `export default ...`
+ *  In the same way, whenever you want to import many things from a file at one
+    time and get access to all of them, can import `* as` + some variable
+    assignment. */
+import * as actions from '../actions';
 
 class ListItem extends Component {
     render() {
@@ -26,4 +36,6 @@ const styles = {
     }
 };
 
-export default ListItem;
+/*  Purpose of the first argument of the connect function is to mapStateToProps.
+ *  Purpose of the second argument is to bind action creators. */
+export default connect(null, actions)(ListItem);
