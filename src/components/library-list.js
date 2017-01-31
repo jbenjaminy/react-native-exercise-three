432d1af32f4cc9e58cefd16231f1a524eeb50055
jbenjaminy <young.benjamin@gmail.com>
2017-01-31 14:15:17 -0600

    adds ListView to library-list component

--- src/components/library-list.js
+++ src/components/library-list.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
-/*
- *  use a connect helper to get access to the redux store in the LibraryList
+/*  use a connect helper to get access to the redux store in the LibraryList
     component.
  *  Theory behind rendering a large list of items:
     *   long list of items, only see the top several and scroll to see more.
@@ -27,17 +27,36 @@ import { connect } from 'react-redux';
             the view from the other end of the screen.
         *   reuses the same set of components over and over and only renders a
             subset of the total items at any given time.
- */
+    *   to summarize, ListView takes a long list of items and renders them in a
+        memory-efficient fashion. */
 
 class LibraryList extends Component {
+    /*  put logic for creating this listView inside of a lifecycle method,
+        `componentWillMount`
+     *  essentially we have this dataSource object and we tell it to take this
+        list of libraries to render the screen. */
+    componentWillMount() {
+        const dataSrc = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+        this.dataSource = dataSrc.cloneWithRows(this.props.libraries);
+    }
+    /* have to instruct the list view how to render a specific row */
+    renderRow() {
+
+    }
+
     render() {
-        console.log(this.props);
-        return;
+        return (
+            <ListView
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
+        );
     }
 }
 
-/*
- *  purpose of 'mapStateToProps' is to take our global application state, and
+/*  purpose of 'mapStateToProps' is to take our global application state, and
     then take some of the properties off of that state object and match them as
     'props' in our component.
 
@@ -46,16 +65,13 @@ class LibraryList extends Component {
 
  *  this libraries piece of state already had some data in it, some amount of
     initial state, therefore we have to design our reducers with the mindset
-    that they will be run over and over again, and at different points in time.
- */
+    that they will be run over and over again, and at different points in time. */
 const mapStateToProps = state => {
     return { libraries: state.libraries };
 };
 
-/*
- *  we call connect and when connect is called it returns another function; we
+/*  we call connect and when connect is called it returns another function; we
     then immediately call that return function, passing the LibraryList to it.
  *  redux concepts are not that difficult to understand -- moreso the terminology,
-    syntax, and implementation.
- */
+    syntax, and implementation. */
 export default connect(mapStateToProps)(LibraryList);
