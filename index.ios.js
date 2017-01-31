22d4bac167be1d8dbc3f922ea333b38ac01f29c6
jbenjaminy <young.benjamin@gmail.com>
2017-01-31 10:09:43 -0600

    installs react & redux and removes contents of index files

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+/*
+    REDUX OVERVIEW
+        1) Action - an object that tells the reducer how to change its data.
+        2) Reducer - a function that returns some data.
+        3) State - data for our app to use.
+        4) Store - an object that holds the application's data.
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+    *By default whenever we get application state with 'store.getState()'
+    the reducer is ran and whatever the reducer returns is our application state.
 
-export default class tech_stack extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+    *Action is a plain JS object that defines a 'type' property (a 'string'), that
+    tells our reducer to complete a particular type of action. Can only modify
+    our application state in very finite ways.
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    * Redux will help you to make a very large app with many features with
+    minimal code.
+*/
 
-AppRegistry.registerComponent('tech_stack', () => tech_stack);
+
+/*
+    REDUX EXAMPLE
+    const reducer = (state = [], action) => () => {
+      if (action.type === 'split_string') {
+        return action.payload.split('');
+      } else if (action.type = 'add_character') {
+        return [ ...state, action.payload ];
+      } else {
+      	return state;
+      }
+    };
+
+    const store = Redux.createStore(reducer);
+
+    store.getState();
+
+    const action = {
+      type: 'split_string',
+      payload: 'asdf'
+     };
+
+    store.dispatch(action);
+
+    store.getState();
+
+    const action2 = {
+      type: 'add_character',
+      payload: 'a'
+    };
+*/
