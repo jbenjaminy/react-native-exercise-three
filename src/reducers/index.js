9baa07a777dcd4d5f09eee87f6904e020e9042e2
jbenjaminy <young.benjamin@gmail.com>
2017-01-31 11:31:40 -0600

    creates library-reducer and library-list files and populates the libraries array in library-list.json

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
-
+import LibraryReducer from './library-reducer';
+/* REDUCERS
+    * reducers produce our application state.
+    * might have many different reducers in our app, and need to get them
+    to all work properly together.
+*/
 /* DIFFERENT TYPES OF REDUCERS - THINKING PROCESS
     - what are the different variables/pieces of data for this app?
         - list of libraries.
@@ -10,13 +15,20 @@ import { combineReducers } from 'redux';
                 { id: 1, name: 'React' },
                 { id: 2, name: 'Redux' }
             ]
-            
+
         - 'selection reducer'
             e.g., 1
 
             this is a separate reducer to just keep track of what item from the
             list to expand (by storing its id).
 */
+
+/*
+Because we set 'LibraryReducer' to this key 'libraries', if we ever
+console.log our state object (e.g., `console.log(store.getState());`), we
+expect the result to be:
+        ` { libraries: [] }`
+*/
 export default combineReducers({
-    libraries: () => []
+    libraries: LibraryReducer
 });
---
+++ src/reducers/library-reducer.js
@@ -0,0 +1,5 @@
+// Data structure is an array of objects, where each individual object
+    // represents an individual library.
+export default () => [
+    
+];
