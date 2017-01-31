dd1f5777454460c193e02903dccdf47acd3f61b8
jbenjaminy <young.benjamin@gmail.com>
2017-01-31 11:41:59 -0600

    imports json data into library-reducer

--- src/reducers/library-reducer.js
+++ src/reducers/library-reducer.js
@@ -1,5 +1,8 @@
 // Data structure is an array of objects, where each individual object
     // represents an individual library.
-export default () => [
-    
-];
+// By default, the package manager we're using assumes that all file extensions
+    // end in '.js'. If we are providing the path to a '.json' or other file, we
+    // need to be very explicit with this path.
+import data from './library-list.json';
+
+export default () => data;
