22ec84eafe362188e8e3b063ddf64984adb12b4b
jbenjaminy <young.benjamin@gmail.com>
2017-01-31 16:07:59 -0600

    adds select_library case in selection-reducer

--- src/reducers/selection-reducer.js
+++ src/reducers/selection-reducer.js
@@ -3,7 +3,17 @@
  *  We can even return null, since its not undefined, and it makes sure that if
     we call this thing at any given time, it does not default to a pre-selected
     row */
-export default (state, action) => {
-    console.log(action);
-    return null;
+export default (state = null, action) => {
+    switch (action.type) {
+        case 'select_library':
+            return action.data;
+        default:
+            return state;
+    }
 };
+
+/*  By convention, every reducer we create is going to have a `switch` statement
+    inside of it that is going to switch over the action.type.
+ *  Need to have a default case and also need to return a non-undefined value
+    for the first time the reducer is run. So must set an initial state default
+    argument `state = null` (as seen in line 6).*/
