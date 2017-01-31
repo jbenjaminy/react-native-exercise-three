439ecb3e662c280b708dfba57f93188aa5bff17c
jbenjaminy <young.benjamin@gmail.com>
2017-01-31 15:20:39 -0600

    creates actions directory and action.js

---
+++ src/actions/actions.js
@@ -0,0 +1,4 @@
+/*  action creators are javascript functions - the component can dispatch an
+    action to be created in our action creator which is then routed to the
+    reducer to update the store of state.
+*   our action creator will create an action and then immediately export it. */
