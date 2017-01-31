5d6330fbd17402df69141ce25ef574b811ab68e6
jbenjaminy <young.benjamin@gmail.com>
2017-01-31 17:34:41 -0600

    refactors render-description function and adds LayoutAnimation to touch event

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -2,7 +2,12 @@
  *  each library item has a title, description and id.
  *  needs to know whether it is selected or not, so must get access to state */
 import React, { Component } from 'react';
-import { TouchableWithoutFeedback, View, Text } from 'react-native';
+import {
+    TouchableWithoutFeedback,
+    View,
+    Text,
+    LayoutAnimation
+} from 'react-native';
 /*  Previously used the connect helper to get access to the application state,
     but can also use the connect helper to get access to an action creator. */
 import { connect } from 'react-redux';
@@ -16,6 +21,26 @@ import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    }
+    
+    renderDescription() {
+        const { library, expanded } = this.props;
+
+        if (expanded) {
+            return (
+                <CardSection>
+                    {/* `flex: 1` here tells the text tag not to wrap itself or
+                        overflow. */}
+                    <Text style={{ flex: 1 }}>
+                        {library.description}
+                    </Text>
+                </CardSection>
+            );
+        }
+    }
+
     /*  ORIGINAL RENDER-DESCRIPTION HELPER FUNCTION:
             `renderDescription() {
                 const { library, selectedLibraryId } = this.props;
@@ -25,6 +50,12 @@ class ListItem extends Component {
                     );
                 }
             }`
+     *  with original mapStateToProps function:
+            `const mapStateToProps = state => {
+                 return { selectedLibraryId: state.selectedLibraryId };
+             };`
+     *  instead, need to pull out the logic and have a single prop that flags
+        when we want to expand something.
     */
     render() {
         const { titleStyle } = styles;
@@ -54,8 +85,17 @@ const styles = {
     }
 };
 
-const mapStateToProps = state => {
-    return { selectedLibraryId: state.selectedLibraryId };
+/*  ownProps are the props that were passed to the component that we're
+    wrapping. ListItem component is passed a library, referenced by
+    `this.props.library`.
+ *  ownProps is exactly equal to `this.props` inside of the component, so any
+    props that are passed to the component will show up inside of
+    mapStateToProps as ownProps.
+ *  By getting access of ownProps inside of mapStateToProps can use some amount
+    of pre-calculation or pre-determination here. */
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+    return { expanded };
 };
 
 /*  Purpose of the first argument of the connect function is to mapStateToProps.
